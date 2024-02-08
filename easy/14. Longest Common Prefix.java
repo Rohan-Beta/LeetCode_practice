bf378372b504a99093e08345ef10d09fd04617bf
Beta <125117740+Rohan-Beta@users.noreply.github.com>
2024-02-08 17:46:34 +0530

    Create 14. Longest Common Prefix.java

---
+++ easy/14. Longest Common Prefix.java
@@ -0,0 +1,33 @@
+// 14. Longest Common Prefix
+
+
+// Example 1:
+
+// Input: strs = ["flower","flow","flight"]
+// Output: "fl"
+// Example 2:
+
+// Input: strs = ["dog","racecar","car"]
+// Output: ""
+// Explanation: There is no common prefix among the input strings.
+
+// solution
+
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+
+        String result = strs[0];
+
+        for(int i = 1; i < strs.length; i += 1) {
+            while(strs[i].indexOf(result) != 0) {
+
+                result = result.substring(0 , result.length() - 1);
+
+                if(result.isEmpty()) {
+                    return "";
+                }
+            }
+        }
+        return result;
+    }
+}
