efa573337635a565366e6d3568c1dbb10c407bf9
Beta <125117740+Rohan-Beta@users.noreply.github.com>
2024-02-10 14:00:19 +0530

    Create *94. Binary Tree Inorder Traversal.java

---
+++ easy/*94. Binary Tree Inorder Traversal.java
@@ -0,0 +1,50 @@
+// 94. Binary Tree Inorder Traversal
+
+//   Input: root = [1,null,2,3]
+// Output: [1,3,2]
+// Example 2:
+
+// Input: root = []
+// Output: []
+// Example 3:
+
+// Input: root = [1]
+// Output: [1]
+
+// solution
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+
+        List<Integer> ans = new ArrayList<>();
+
+        Deque<TreeNode> stack = new ArrayDeque<>();
+
+        while(root != null || !stack.isEmpty()) {
+            while(root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            ans.add(root.val);
+
+            root = root.right;
+        }
+        return ans;
+    }
+}
