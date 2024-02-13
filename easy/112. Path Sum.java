8fa0ff123ebc8598e1a5d447be3f7e20d7122b73
Beta <125117740+Rohan-Beta@users.noreply.github.com>
2024-02-13 12:29:44 +0530

    Create 112. Path Sum.java

---
+++ easy/112. Path Sum.java
@@ -0,0 +1,45 @@
+// 112. Path Sum
+
+//   Input: root = [5,4,8,11,null,13,4,7,2,null,null,null,1], targetSum = 22
+// Output: true
+// Explanation: The root-to-leaf path with the target sum is shown.
+
+//   Input: root = [1,2,3], targetSum = 5
+// Output: false
+// Explanation: There two root-to-leaf paths in the tree:
+// (1 --> 2): The sum is 3.
+// (1 --> 3): The sum is 4.
+// There is no root-to-leaf path with sum = 5.
+
+// solution
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+
+        if(root == null) {
+            return false;
+        }
+        if(root.val == targetSum && root.left == null && root.right == null) {
+            return true;
+        }
+        return(
+            hasPathSum(root.left , targetSum - root.val) ||
+            hasPathSum(root.right , targetSum - root.val)
+        );
+    }
+}
