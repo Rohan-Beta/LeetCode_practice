def7caa270a7093402426e2ce7b753c6b99d0122
Beta <125117740+Rohan-Beta@users.noreply.github.com>
2024-02-15 13:03:43 +0530

    Create 182. Duplicate Emails.sql

---
+++ easy/182. Duplicate Emails.sql
@@ -0,0 +1,24 @@
+-- Example 1:
+
+-- Input: 
+-- Person table:
+-- +----+---------+
+-- | id | email   |
+-- +----+---------+
+-- | 1  | a@b.com |
+-- | 2  | c@d.com |
+-- | 3  | a@b.com |
+-- +----+---------+
+-- Output: 
+-- +---------+
+-- | Email   |
+-- +---------+
+-- | a@b.com |
+-- +---------+
+-- Explanation: a@b.com is repeated two times.
+
+-- solution
+
+SELECT email FROM Person
+GROUP BY email
+HAVING COUNT(email) > 1;
