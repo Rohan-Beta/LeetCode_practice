d94138cb53ac1a5967e43a0086b1b7c04103a99d
Beta <125117740+Rohan-Beta@users.noreply.github.com>
2024-02-15 12:51:52 +0530

    Create 181. Employees Earning More Than Their Managers.sql

---
+++ easy/181. Employees Earning More Than Their Managers.sql
@@ -0,0 +1,27 @@
+-- Input: 
+-- Employee table:
+-- +----+-------+--------+-----------+
+-- | id | name  | salary | managerId |
+-- +----+-------+--------+-----------+
+-- | 1  | Joe   | 70000  | 3         |
+-- | 2  | Henry | 80000  | 4         |
+-- | 3  | Sam   | 60000  | Null      |
+-- | 4  | Max   | 90000  | Null      |
+-- +----+-------+--------+-----------+
+-- Output: 
+-- +----------+
+-- | Employee |
+-- +----------+
+-- | Joe      |
+-- +----------+
+
+-- # Write your MySQL query statement below
+
+SELECT e1.name AS `Employee`
+FROM
+    Employee AS `e1`,
+    Employee AS `e2`
+WHERE
+    e1.managerId = e2.id
+    AND e1.salary > e2.salary;
+
