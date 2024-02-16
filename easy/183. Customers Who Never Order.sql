bc76ed5786ca52caf566727c2dee6e443e5e36dc
Beta <125117740+Rohan-Beta@users.noreply.github.com>
2024-02-16 20:48:18 +0530

    Create 183. Customers Who Never Order.sql

---
+++ easy/183. Customers Who Never Order.sql
@@ -0,0 +1,38 @@
+-- 183. Customers Who Never Order
+
+-- Example 1:
+
+-- Input: 
+-- Customers table:
+-- +----+-------+
+-- | id | name  |
+-- +----+-------+
+-- | 1  | Joe   |
+-- | 2  | Henry |
+-- | 3  | Sam   |
+-- | 4  | Max   |
+-- +----+-------+
+-- Orders table:
+-- +----+------------+
+-- | id | customerId |
+-- +----+------------+
+-- | 1  | 3          |
+-- | 2  | 1          |
+-- +----+------------+
+-- Output: 
+-- +-----------+
+-- | Customers |
+-- +-----------+
+-- | Henry     |
+-- | Max       |
+-- +-----------+
+
+-- solution
+
+# Write your MySQL query statement below
+
+SELECT name AS `Customers`
+FROM Customers
+WHERE id NOT IN(
+    SELECT customerId FROM Orders
+);
