6c1c63b776ab874e2248c8d9ee981d7e49e36c94
Beta <125117740+Rohan-Beta@users.noreply.github.com>
2024-02-17 11:34:39 +0530

    Create *197. Rising Temperature.sql

---
+++ easy/*197. Rising Temperature.sql
@@ -0,0 +1,33 @@
+-- 197. Rising Temperature
+
+-- Example 1:
+
+-- Input: 
+-- Weather table:
+-- +----+------------+-------------+
+-- | id | recordDate | temperature |
+-- +----+------------+-------------+
+-- | 1  | 2015-01-01 | 10          |
+-- | 2  | 2015-01-02 | 25          |
+-- | 3  | 2015-01-03 | 20          |
+-- | 4  | 2015-01-04 | 30          |
+-- +----+------------+-------------+
+-- Output: 
+-- +----+
+-- | id |
+-- +----+
+-- | 2  |
+-- | 4  |
+-- +----+
+-- Explanation: 
+-- In 2015-01-02, the temperature was higher than the previous day (10 -> 25).
+-- In 2015-01-04, the temperature was higher than the previous day (20 -> 30).
+
+-- solution
+
+# Write your MySQL query statement below
+
+SELECT W1.id
+FROM Weather W1 , Weather W2
+WHERE W1.temperature > W2.temperature
+AND DATEDIFF(W1.recordDate , W2.recordDate) = 1;
