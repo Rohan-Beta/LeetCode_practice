9ddcf68ca44604c52f25fa30eb79a02c9cd61785
Beta <125117740+Rohan-Beta@users.noreply.github.com>
2024-02-17 12:00:14 +0530

    Create *205. Isomorphic Strings.java

---
+++ easy/*205. Isomorphic Strings.java
@@ -0,0 +1,31 @@
+// 205. Isomorphic Strings
+
+//   Example 1:
+
+// Input: s = "egg", t = "add"
+// Output: true
+// Example 2:
+
+// Input: s = "foo", t = "bar"
+// Output: false
+// Example 3:
+
+// Input: s = "paper", t = "title"
+// Output: true
+
+// solution
+
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+
+        Map<Character , Integer> charIndex_s = new HashMap<>();
+        Map<Character , Integer> charIndex_t = new HashMap<>();
+
+        for(Integer i = 0; i < s.length(); i += 1) {
+            if(charIndex_s.put(s.charAt(i) , i) != charIndex_t.put(t.charAt(i) , i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
