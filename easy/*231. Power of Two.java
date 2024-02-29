2dc6f7f13b136230829ac3f99be42d45ad839774
Beta <125117740+Rohan-Beta@users.noreply.github.com>
2024-02-29 13:59:28 +0530

    Update *231. Power of Two.java

--- easy/*231. Power of Two.java
+++ easy/*231. Power of Two.java
@@ -37,6 +37,12 @@ class Solution {
 class Solution {
     public boolean isPowerOfTwo(int n) {
 
-        return n < 0? false : Integer.bitCount(n) == 1;
+        while(n >= 2) {
+            if(n % 2 != 0) {
+                return false;
+            }
+            n /= 2;
+        }
+        return n == 1? true : false;
     }
 }
