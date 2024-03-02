51b96ae1228e3e5d6857fafea7fcc8b46edf6aa3
Beta <125117740+Rohan-Beta@users.noreply.github.com>
2024-03-02 11:14:55 +0530

    Create *404. Sum of Left Leaves.java

---
+++ easy/*404. Sum of Left Leaves.java
@@ -0,0 +1,48 @@
+// 404. Sum of Left Leaves
+  
+// Input: root = [3,9,20,null,null,15,7]
+// Output: 24
+// Explanation: There are two left leaves in the binary tree, with values 9 and 15 respectively.
+// Example 2:
+
+// Input: root = [1]
+// Output: 0
+
+// solution
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int sumOfLeftLeaves(TreeNode root) {
+
+        if(root == null) {
+            return 0;
+        }
+        int ans = 0;
+
+        if(root.left != null) {
+            if(root.left.left == null && root.left.right == null) {
+                ans += root.left.val;
+            }
+            else {
+                ans += sumOfLeftLeaves(root.left);
+            }
+        }
+        ans += sumOfLeftLeaves(root.right);
+
+        return ans;
+    }
+}
