fd80c6d3ee64234651e85bd25db9415a539b9c3b
Beta <125117740+Rohan-Beta@users.noreply.github.com>
2024-05-02 12:48:32 +0530

    Create 392. Is Subsequence.java

---
+++ Top Interview 150/392. Is Subsequence.java
@@ -0,0 +1,30 @@
+// 392. Is Subsequence
+
+//   Example 1:
+
+// Input: s = "abc", t = "ahbgdc"
+// Output: true
+// Example 2:
+
+// Input: s = "axc", t = "ahbgdc"
+// Output: false
+
+// solution
+
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+
+        if(s.isEmpty()) {
+            return true;
+        }
+        int i = 0;
+
+        for(char ch : t.toCharArray()) {
+
+            if(s.charAt(i) == ch && ++i == s.length()) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
