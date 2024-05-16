adaf02ca1d5e0bd4ddda26339a4d33013a568b95
Beta <125117740+Rohan-Beta@users.noreply.github.com>
2024-05-16 11:31:13 +0530

    Create 155. Min Stack.java

---
+++ Top Interview 150/155. Min Stack.java
@@ -0,0 +1,61 @@
+// 155. Min Stack
+
+//   Example 1:
+
+// Input
+// ["MinStack","push","push","push","getMin","pop","top","getMin"]
+// [[],[-2],[0],[-3],[],[],[],[]]
+
+// Output
+// [null,null,null,null,-3,null,0,-2]
+
+// Explanation
+// MinStack minStack = new MinStack();
+// minStack.push(-2);
+// minStack.push(0);
+// minStack.push(-3);
+// minStack.getMin(); // return -3
+// minStack.pop();
+// minStack.top();    // return 0
+// minStack.getMin(); // return -2
+
+// solution
+
+class MinStack {
+    Stack<int[]> stack = new Stack<>();
+
+    public MinStack() {
+        
+    }
+    
+    public void push(int val) {
+
+        if(stack.isEmpty()) {
+            stack.push(new int[] {val , val});
+        }
+        else {
+            stack.push(new int[] {val , Math.min(val , stack.peek()[1])});
+        }
+    }
+    
+    public void pop() {
+        stack.pop();
+    }
+    
+    public int top() {
+        return stack.peek()[0];
+    }
+    
+    public int getMin() {
+        return stack.peek()[1];
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
