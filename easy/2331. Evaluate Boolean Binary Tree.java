5acf87d5c8d48c937352b086be319eaa1ee40d12
Beta <125117740+Rohan-Beta@users.noreply.github.com>
2024-05-16 10:44:38 +0530

    Create 2331. Evaluate Boolean Binary Tree.java

---
+++ easy/2331. Evaluate Boolean Binary Tree.java
@@ -0,0 +1,43 @@
+// 2331. Evaluate Boolean Binary Tree
+
+//   Input: root = [2,1,3,null,null,0,1]
+// Output: true
+// Explanation: The above diagram illustrates the evaluation process.
+// The AND node evaluates to False AND True = False.
+// The OR node evaluates to True OR False = True.
+// The root node evaluates to True, so we return true.
+// Example 2:
+
+// Input: root = [0]
+// Output: false
+// Explanation: The root node is a leaf node and it evaluates to false, so we return false.
+
+// solution
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean evaluateTree(TreeNode root) {
+
+        if(root.val < 2) {
+            return root.val == 1 ? true : false;
+        }
+        if(root.val == 2) {
+            return evaluateTree(root.left) || evaluateTree(root.right);
+        }
+        return evaluateTree(root.left) && evaluateTree(root.right);
+    }
+}
